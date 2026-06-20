94f7d5dc06d89b6699858872c311f63c2a4dcb14
Semen Payusov <semenpausov@gmail.com>
2026-06-21 01:42:21 +0300

    feat(profile): добавить модуль профиля студента

---
+++ profile.py
@@ -0,0 +1,13 @@
+def get_profile():
+    return {
+        "name": "Payusov Semen",      
+        "group": "RPO-1",           
+        "role": "student",
+        "skills": ["Git", "VS Code", "Python"]
+    }
+
+def print_profile():
+    profile = get_profile()
+    print(f"Студент: {profile['name']}")
+    print(f"Группа: {profile['group']}")
+    print("Навыки:", ", ".join(profile["skills"]))
